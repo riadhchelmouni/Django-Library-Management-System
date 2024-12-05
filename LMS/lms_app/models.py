d2a4437ecf4fdab0913e6b3c63c9abdf427194ac
riadhchelmouni <chelmouni.yazidriadh@gmail.com>
2024-12-05 23:14:14 +0100

    first commit

--- LMS/lms_app/models.py
+++ LMS/lms_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    year = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+  
