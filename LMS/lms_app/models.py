1e874f1dbcbb5b31dbe206a5c11d94780871c47d
riadhchelmouni <chelmouni.yazidriadh@gmail.com>
2024-12-06 12:18:38 +0100

    first commit

--- LMS/lms_app/models.py
+++ LMS/lms_app/models.py
@@ -1,21 +1,30 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-
     def __str__(self):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
+    
+    status_book = [
+        ('available', 'available'),
+        ('rental', 'rental'),
+        ('sold', 'sold'),
+    ]
+    
+    title = models.CharField(max_length=250)
     author = models.CharField(max_length=100)
-    year = models.IntegerField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
+    photo_book = models.ImageField(upload_to='photos', null=True, blank=True)
+    photo_author = models.ImageField(upload_to='photos', null=True, blank=True)
+    pages = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    retal_price_day = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    retal_period = models.IntegerField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    status = models.CharField(max_length=100, choices=status_book,null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    
     def __str__(self):
         return self.title
-  
